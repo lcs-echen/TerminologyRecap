019da202cc83ab2cd15cd4ebb3c8115848842f58
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-01-26 09:35:51 -0500

    Initial Commit

---
+++ TerminologyRecap/TerminologyRecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminologyRecapApp.swift
+//  TerminologyRecap
+//
+//  Created by Evelyn Chen on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct TerminologyRecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
